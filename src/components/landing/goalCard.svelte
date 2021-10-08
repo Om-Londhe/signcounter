7db7571c0a93799ec55bf04c58cf82fade852455
DevOM <oplondhe@gmail.com>
2021-10-08 15:52:35 +0530

    After completing Goals section

--- src/components/landing/goalCard.svelte
+++ src/components/landing/goalCard.svelte
@@ -4,16 +4,93 @@
 </script>
 
 <div class="goalCard">
-	<h1>{goalNumber}</h1>
-	<h3>{content}</h3>
+	<h2 class="number">0{goalNumber}</h2>
+	<h3 class="title">Goal {goalNumber}</h3>
+	<p class="content">{content}</p>
 </div>
 
 <style>
+	@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@600&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Varela+Round&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Baloo+Paaji+2&display=swap');
 	.goalCard {
-		height: 250px;
+		height: 275px;
 		width: 200px;
-		border: 1px solid red;
-		border-radius: 11px;
 		padding: 7px;
+		position: relative;
+		box-shadow: 20px 20px 50px rgba(0, 0, 0, 0.5);
+		border-radius: 15px;
+		background: rgba(255, 255, 255, 0.1);
+		overflow: hidden;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		border-top: 1px solid rgba(255, 255, 255, 0.5);
+		border-left: 1px solid rgba(255, 255, 255, 0.5);
+		backdrop-filter: blur(4px);
+		padding: 20px;
+		text-align: center;
+		transition: 0.5s;
+		cursor: none;
+		user-select: none;
+		margin: 11px;
+	}
+	.number {
+		position: absolute;
+		top: -20px;
+		right: 20px;
+		font-size: 8em;
+		color: rgba(255, 255, 255, 0.05);
+		font-family: 'Raleway', sans-serif;
+		transition: 0.7s;
+	}
+	.title {
+		font-size: 24px;
+		color: black;
+		z-index: 1;
+		font-family: 'Varela Round', sans-serif;
+		margin-bottom: 11px;
+		background: whitesmoke;
+		border-radius: 51px 21px;
+		padding: 21px;
+		padding-top: 7px;
+		padding-bottom: 7px;
+	}
+	.content {
+		font-size: 18px;
+		color: white;
+		font-weight: 300;
+		font-family: 'Baloo Paaji 2', cursive;
+	}
+	@media only screen and (min-width: 750px) {
+		.number {
+			top: calc(35% - 40px);
+			right: calc(35% - 40px);
+			color: whitesmoke;
+		}
+		.goalCard:hover .number {
+			top: -20px;
+			right: 20px;
+			color: rgba(255, 255, 255, 0.05);
+		}
+		.title {
+			transform: translateY(-150px);
+			transition: 0.7s;
+			opacity: 0;
+		}
+		.goalCard:hover .title {
+			transform: translateY(0);
+			opacity: 1;
+		}
+		.content {
+			transform: translateY(200px);
+			transition: 0.7s;
+			opacity: 0;
+		}
+		.goalCard:hover .content {
+			transform: translateY(0);
+			opacity: 1;
+		}
 	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,27 @@
 	import Footer from '../components/landing/footer.svelte';
 
 	let scrollY;
+	let goalCardData = [
+		{
+			goalNumber: 1,
+			content: 'To protect you online by protecting your CounterSigns/Passwords.'
+		},
+		{
+			goalNumber: 2,
+			content:
+				'To not let you go out of memory by remembering your passwords for any login able thing.'
+		},
+		{
+			goalNumber: 3,
+			content:
+				'To provide a single, dedicated & trustworthy place to store your CounterSigns/Passwords.'
+		},
+		{
+			goalNumber: 4,
+			content:
+				'To keep your credentials secure from those online thieves who can attack you anytime.'
+		}
+	];
 </script>
 
 <svelte:window bind:scrollY />
@@ -21,10 +42,9 @@
 		<div class="goalsContainer">
 			<h2 class="goalTitle">Our Goals</h2>
 			<div class="goals">
-				<GoalCard
-					goalNumber={1}
-					content="To protect you online by protecting your CounterSigns/Passwords."
-				/>
+				{#each goalCardData as goalCard (goalCard.goalNumber)}
+					<GoalCard goalNumber={goalCard.goalNumber} content={goalCard.content} />
+				{/each}
 			</div>
 		</div>
 	</div>
@@ -46,6 +66,34 @@
 		padding-top: 111px;
 		height: 500vh;
 	}
+	.goalsContainer {
+		width: 100%;
+		padding-top: 121px;
+		padding-bottom: 111px;
+		background: url('/blob-scene.svg');
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: cover;
+		position: relative;
+	}
+	.goalsContainer::before {
+		content: '';
+		position: absolute;
+		top: -1px;
+		background: linear-gradient(#1c0522, transparent);
+		height: 150px;
+		width: 100%;
+		z-index: 1;
+	}
+	.goalsContainer::after {
+		content: '';
+		position: absolute;
+		bottom: -1px;
+		background: linear-gradient(transparent, #1c0522);
+		height: 150px;
+		width: 100%;
+		z-index: 1;
+	}
 	.goalTitle {
 		font-size: 21px;
 		font-family: 'Merienda', cursive;
@@ -58,9 +106,24 @@
 		width: 150px;
 		text-align: center;
 		margin-left: calc(50% - 75px);
-	}
-	.goalsContainer {
+		margin-bottom: 21px;
+		z-index: 5;
 	}
 	.goals {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-wrap: wrap;
+	}
+	@media only screen and (max-width: 675px) {
+		.goalsContainer {
+			background: url('/blob-scene-tab.svg');
+		}
+	}
+	@media only screen and (max-width: 525px) {
+		.goalsContainer {
+			background: url('/blob-scene-mobile.svg');
+			padding-bottom: 60px;
+		}
 	}
 </style>
